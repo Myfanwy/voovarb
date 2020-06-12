e7f3fde7c58280b9b0f897f61c9b65dd905f297a
Myfanwy <myfanwy@github.com>
2020-06-12 16:31:05 -0700

    test gitignore

--- R/write_gitignore.R
+++ R/write_gitignore.R
@@ -1,19 +1,19 @@
 #'@export
-#'
-write_gitignore = function() {
-    cat(".Rhistory
-.Rproj.*
-*.hpp
-*.o
-*.so
-*.hpp
-inst/doc
-doc
-Meta
-.Rproj.user",
+ignore_items = c(".Rhistory", ".Rproj.*", "*.o", "*.so")
 
-  file = ".gitignore", sep = "\n"
-  )
+#'Make a gitignore file
+#'@description A function to write a .gitignore file with common ignore items
+#'@details To see the vector of default things added to .gitignore, call voovarb:::ignore_items
+#'@author Myfanwy Johnston
+#'@author Matt Espe
+#'@export
+
+write_gitignore = function(ignore = ignore_items,
+                           file = ".gitignore",
+                          add = file.exists(file)) {
+  if(add)
+       ignore = setdiff(ignore, readLines(file))
+
+   cat(ignore, file = file , sep = "\n", append = add)
 
 }
-
